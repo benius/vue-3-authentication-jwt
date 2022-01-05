bbb9c380adfcb4805a35f378b4e1571bd3d538be
Mason Hsieh <benius@gmail.com>
2022-01-05 18:24:09 +0800

    remain content in js but support ts

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <a href="/" class="navbar-brand">bezKoder</a>
+      <div class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/home" class="nav-link">
+            <font-awesome-icon icon="home"/>
+            Home
+          </router-link>
+        </li>
+        <li v-if="showAdminBoard" class="nav-item">
+          <router-link to="/admin" class="nav-link">Admin Board</router-link>
+        </li>
+        <li v-if="showModeratorBoard" class="nav-item">
+          <router-link to="/mod" class="nav-link">Moderator Board</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>
+        </li>
+      </div>
+
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">
+            <font-awesome-icon icon="user-plus"/>
+            Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt"/>
+            Login
+          </router-link>
+        </li>
+      </div>
+
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+            <font-awesome-icon icon="user"/>
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click.prevent="logOut">
+            <font-awesome-icon icon="sign-out-alt"/>
+            LogOut
+          </a>
+        </li>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld,
+import { computed, defineComponent } from "vue";
+import store from "./store";
+import router from "@/router";
+
+export default defineComponent({
+  setup() {
+    const currentUser = computed(() => store.state.auth.user);
+    const showAdminBoard = computed(() => (currentUser.value && currentUser.value['roles']) ? currentUser.value['roles'].includes('ROLE_ADMIN') : false);
+    const showModeratorBoard = computed(() => (currentUser.value && currentUser.value['roles']) ? currentUser.value['roles'].include('ROLE_MODERATOR') : false);
+
+    return {
+      currentUser, showAdminBoard, showModeratorBoard
+    }
   },
+  methods: {
+    logOut() {
+      store.dispatch('auth/logout');
+      router.push('login');
+    }
+  }
 })
-export default class App extends Vue {}
 </script>
 
 <style>
